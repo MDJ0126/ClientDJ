b2b85b25253f4ad36b30eed7b2c03026da64e75e
문동준 <ehdwns0126@gmail.com>
2023-08-21 03:16:56 +0900

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
+var particle_json_path = '/assets/json/particles.json' | relative_url
 document.addEventListener(
   "DOMContentLoaded",
   (event) => {
-    particlesJS.load("particles-js", '/assets/json/particles.json' | relative_url, function () {
+    particlesJS.load("particles-js", particle_json_path, function () {
       console.log("particles.js loaded - callback");
     });
   },
